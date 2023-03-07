baf8b938c0cb84e6d891366a5fb21bfa8192077b
jepery <jeperydcruz@gmail.com>
2023-03-07 19:36:26 +0800

    add updates

---
+++ js/myscript.js
@@ -0,0 +1,27 @@
+let hero = 'Spiderman';
+
+switch (hero) {
+
+    case 'Superman':
+        console.log('Super strength');
+        console.log('X-ray vision');
+
+    case 'Batman':
+        console.log('Money and intelliegence');
+        console.log('Martial Arts');
+
+    case 'Auquaman':
+        console.log('Can breath under water');
+        console.log('Can communicate with fishes');
+        break;
+        default:
+            if( hero == 'Green Arrow'){
+                console.log('Sharp shooter Billionare');
+            }
+            else{
+                console.log('')
+            }
+
+            break;
+
+}
